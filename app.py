d7140f83c8e2bd46f4731eb3c280add264975042
shivanirajora10 <shivanirajora37810@gmail.com>
2025-01-21 18:27:03 +0530

    Add functionality to download similarity.pkl

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pickle
 import pandas as pd
 import requests
-
+import os
+import urllib.request
 
 # Function to fetch movie poster from OMDb API
 def fetch_poster(movie_title):
@@ -14,7 +15,6 @@ def fetch_poster(movie_title):
         return data['Poster']
     else:
         return "https://via.placeholder.com/300x450?text=No+Poster+Found"
-
 
 # Function to recommend movies
 def recommend(movie):
@@ -32,11 +32,17 @@ def recommend(movie):
         recommended_posters.append(poster_url)
     return recommended_movies, recommended_posters
 
-
 # Load movie data and similarity matrix
 movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
-similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+# Check if similarity.pkl exists, otherwise download it
+file_path = 'similarity.pkl'
+if not os.path.exists(file_path):
+    url = "https://drive.google.com/uc?id=1_XWfuxX8g4NtT0OpU0bieU7CDpHbWkp5&export=download"
+    urllib.request.urlretrieve(url, file_path)
+
+similarity = pickle.load(open(file_path, 'rb'))
 
 # Streamlit UI
 st.title('Movie Recommender System')
@@ -49,34 +55,28 @@ if st.button('Recommend'):
 
     col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
-
         st.image(posters[0])
         st.text(recommendations[0])
 
     with col2:
-
-            st.image(posters[1])
-            st.text(recommendations[1])
+        st.image(posters[1])
+        st.text(recommendations[1])
 
     with col3:
-
         st.image(posters[2])
         st.text(recommendations[2])
 
     with col4:
-
         st.image(posters[3])
         st.text(recommendations[3])
 
     with col5:
-
         st.image(posters[4])
         st.text(recommendations[4])
 
     # Create 5 columns for the recommendations
     #cols = st.columns(5)
-   # for col, movie, poster in zip(cols, recommendations, posters):
-        #with col:
-           # st.image(poster, use_container_width=True)  # Display the poster
-           # st.write(movie)  # Display the movie title
-
+    # for col, movie, poster in zip(cols, recommendations, posters):
+    #     with col:
+    #         st.image(poster, use_container_width=True)  # Display the poster
+    #         st.write(movie)  # Display the movie title
